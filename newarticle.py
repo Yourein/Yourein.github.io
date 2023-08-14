a804ae4485c9ac2dfa4bb0c0b2fab19b83655c4b
Yourein <rein.you.3.1@gmail.com>
2023-08-14 19:05:32 +0900

    Create a script to generate a templete article

---
+++ newarticle.py
@@ -0,0 +1,16 @@
+import hashlib
+from datetime import datetime
+import os
+
+hasher = hashlib.sha256()
+now = datetime.now()
+
+article_name_prefix = now.strftime("%Y-%m-%d")
+hasher.update(now.strftime("%Y-%m-%d-%H:%M:%S").encode())
+article_name = article_name_prefix + '-' + hasher.hexdigest()[:6]
+
+cwd = os.getcwd()
+with open(cwd + "/_posts/" + article_name + ".md", 'a') as f:
+    title = input("Enter the title: ").rstrip("\n")
+
+    f.write(f"---\nlayout: page\ntitle: {title}\n---\n")
