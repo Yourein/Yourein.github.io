e6441d85b785867b490b8b0f92f5cd4fdfb7f322
Yourein <rein.you.3.1@gmail.com>
2023-09-21 11:42:15 +0900

    タイトルを入力中にInterruptしたときファイルが作成されていた不具合を修正

--- newarticle.py
+++ newarticle.py
@@ -8,9 +8,8 @@ now = datetime.now()
 article_name_prefix = now.strftime("%Y-%m-%d")
 hasher.update(now.strftime("%Y-%m-%d-%H:%M:%S").encode())
 article_name = article_name_prefix + '-' + hasher.hexdigest()[:6]
+title = input("Enter the title: ").rstrip("\n")
 
 cwd = os.getcwd()
 with open(cwd + "/_posts/" + article_name + ".md", 'a') as f:
-    title = input("Enter the title: ").rstrip("\n")
-
     f.write(f"---\nlayout: page\ntitle: {title}\n---\n")
